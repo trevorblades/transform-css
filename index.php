bc7de0faee29c2650f4dd76705ae0b0526ac5b6b
Trevor Blades <tdblades@gmail.com>
2014-08-24 13:22:10 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Fishsticss</title>
+	<meta property="og:title" content="Fishsticss - Making your stylesheets cool again" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="http://www.fishsticss.com/images/fb.png" />
+	<meta property="og:description" content="Fishsticss takes your old school CSS files and converts them to beautiful, functional LESS files. Take the leap into the future and start using LESS today!">
+	<meta property="og:site_name" content="Fishsticss" />
+	<meta property="og:url" content="http://www.fishsticss.com" />
+	<meta property="fb:admins" content="502786709" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/normalize.css" type="text/css">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-34654229-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+</head>
+<body>
+	<img src="images/design/logo.png" id="logo">
+	<ul id="menu">
+		<li>
+			<a href="#comments" class="on">Preserve comments:<span>On</span></a>
+		</li>
+		<li>
+			<a href="#variables">Create color variables:<span>Off</span></a>
+		</li>
+		<li>
+			<a href="#convert" class="select">Convert to: 
+				<ul>
+					<li data-value="false">None</li>
+					<li data-value="less" class="selected">LESS</li>
+					<li data-value="sass">SASS</li>
+					<li data-value="scss">SCSS</li>
+				</ul>
+			</a>
+		</li>
+	</ul>
+	<table>
+		<tr>
+			<td>
+				<textarea id="input">/* Comments ain't no thang! */
+
+body {
+	width: 0px;
+	color: #000;
+}
+body #div {
+	width: 100%;
+}
+</textarea>
+			</td>
+			<td>
+				<textarea id="output"></textarea>
+			</td>
+		</tr>
+	</table>
+	<div id="footer">
+		<span>&copy; <?php echo date('Y'); ?> <a href="http://www.trevorblad.es" target="_blank">Trevor Blades</a></span>
+		<span class="attr"><a href="http://thenounproject.com/noun/fish-skeleton/#icon-No3167" target="_blank">Fish Skeleton</a> designed by <a href="http://thenounproject.com/gilad1" target="_blank">Gilad Fried</a> from The Noun Project</span>
+	</div>
+	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="js/fishsticss.js"></script>
+	<script type="text/javascript" src="js/index.js"></script>
+</body>
+</html>
