cfc2adb3c90db1286e86288d7cd79d7964153ff3
Trevor Blades <tdblades@gmail.com>
2016-02-23 09:13:29 -0800

    Update regex and organize decendant selectors

--- src/fishsticss.js
+++ src/fishsticss.js
@@ -1,8 +1,8 @@
 
 // TODO: prevent capturing spaces at the end of a match so we don't use trim() everywhere
 var patterns = {
-  selector: /([_a-zA-Z0-9-:,#:.\[\]="\s]?[_a-zA-Z0-9-*:>+,#:.\(\)\[\]=|~^$"\s]*)\s*\{\s*([\S\s]*?)\s*\}/gm,
-  rule: /(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)\s*:\s*([_a-zA-Z0-9-%#'",.\(\)\s]+);*/gm,
+  selector: /(.+?){\s*([\S\s]*?)\s*\}/gm,
+  rule: /(.+?):(.+?);*/gm,
   comment: /\/\*[\S\s]*\*\//gm
 };
 
@@ -10,7 +10,7 @@ var Fishsticss = function(options) {
   return {
     scrub: function(css) {
 
-      var selectors = {};
+      var styles = {};
       var match = patterns.selector.exec(css);
       while (match) {
 
@@ -27,15 +27,34 @@ var Fishsticss = function(options) {
           return a;
         }, {});
 
-        if (selectors[selector]) {
-          selectors[selector].rules = Object.assign(selectors[selector].rules, rules);
+        if (styles[selector]) {
+          styles[selector].rules = Object.assign(styles[selector].rules, rules);
         } else {
-          selectors[selector] = {rules: rules};
+          styles[selector] = {rules: rules};
         }
 
         match = patterns.selector.exec(css);
       }
-      return selectors;
+
+      var selectors = Object.keys(styles);
+      for (var key in styles) {
+        var selectorIndex = selectors.findIndex(function(selector) {
+          return key !== selector && key.indexOf(selector) > -1;
+        });
+        if (selectorIndex > -1) {
+          var parentSelector = selectors[selectorIndex];
+          if (key !== parentSelector) {
+            if (!styles[parentSelector].children) {
+              styles[parentSelector].children = {};
+            }
+            styles[parentSelector]
+                .children[key.replace(parentSelector, '').trim()] = styles[key];
+            delete styles[key];
+          }
+        }
+      }
+
+      return styles;
     }
   };
 };
