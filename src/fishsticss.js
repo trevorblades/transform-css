621d54c30b219a834d2a98fedb3328e8f088ba85
Robert Nash <robert.nash@4-roads.com>
2016-03-05 14:49:25 +0000

    support for media queries

--- src/fishsticss.js
+++ src/fishsticss.js
@@ -6,83 +6,87 @@ var COMMENT_PATTERN = /(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g
 var COLOR_PATTERN = /(?:#((?:[0-9a-f]{3}){1,2})|((?:rgb|hsl)a?)\((25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,\s*(25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,\s*(25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*(?:,\s*(1(?:\.0)?|(?:0|0?\.\d\d?)))?\))/gim;
 var STYLE_SPLIT_PATTERN = /((?:[^;"']|"[^"]*"|'[^']*')+)/gm;
 var RULE_SPLIT_PATTERN = /((?:[^:"']|"[^"]*"|'[^']*')+)/gm;
+var MEDIA_PATTERN = /\@media[^\{]*\{([^\{\}]*\{[^\}\{]*\})*[^\}]*\}/gm;
+var FONTFACE_PATTERN = /\@fontface[^\{]*\{([^\{\}]*\{[^\}\{]*\})*[^\}]*\}/gm;
 
 var fishsticss = {
 
-  _scrape: function(css) {
+  _scrape: function(css , commentMap) {
 
     var styles = {};
     var colors = {};
-
 
     // TODO: use css.match(SELECTOR_PATTERN) instead
     var match = SELECTOR_PATTERN.exec(css);
     while (match) {
 
-      var selector = match[1].trim();
-
-      //remove any comments 
-      var cssStyles = match[2].trim();
-      var commentFiltered = cssStyles.replace(COMMENT_PATTERN, '\n');
-
-      // Split apart the style properties
-      // TODO: use a fat arrow function for this filter?
-      var rules = commentFiltered.split(STYLE_SPLIT_PATTERN).filter(function(rule) {
-        return rule;
-      }).reduce(function(a, b) {
-
-        var itt = 1;
-        var rule = b.split(RULE_SPLIT_PATTERN).filter(function(rule) {
-          if (itt++ %2 == 0){
-            return rule;
-          }
-          return '';
-        });
-        if (rule.length > 1) {
-
-          var value = rule[1].trim();
-
-          // Check if the value is a color (hex, rgb, or hsl)
-          var colorMatch = COLOR_PATTERN.exec(value);
-          if (colorMatch) {
-
-            // Format the color appropriately
-            // TODO: make color formatting optional
-            var color = new Color(colorMatch[0]);
-            var colorString = color.alpha() === 1 ?
-                color.hexString().toLowerCase() : color.rgbString();
-
-            // Increment count for the color
-            if (!colors[colorString]) {
-              colors[colorString] = 0;
-            }
-            colors[colorString] += 1;
-            value = value.replace(colorMatch[0], colorString);
-          }
-
-          a[rule[0].trim()] = value;
-        }
-        return a;
-      }, {});
-
-      //store the comments
-      var internalComments = [];
-      var commentMatch = COMMENT_PATTERN.exec(cssStyles);
-
-      while (commentMatch) {
-        internalComments.push(commentMatch[0].trim());
-        commentMatch = COMMENT_PATTERN.exec(cssStyles);
-      }
-
-      // Assign styles to the selector or define a new selector
-      if (styles[selector]) {
-        styles[selector].rules = Object.assign(styles[selector].rules, rules);
-      } else {
-        styles[selector] = {
-          index: match.index,
-          rules: rules,
-          internalComments:internalComments
-        };
+      if (commentMap[match.index] == undefined){
+    	
+	      var selector = match[1].trim();
+	
+	      //remove any comments 
+	      var cssStyles = match[2].trim();
+	      var commentFiltered = cssStyles.replace(COMMENT_PATTERN, '\n');
+	
+	      // Split apart the style properties
+	      // TODO: use a fat arrow function for this filter?
+	      var rules = commentFiltered.split(STYLE_SPLIT_PATTERN).filter(function(rule) {
+	        return rule;
+	      }).reduce(function(a, b) {
+	
+	        var itt = 1;
+	        var rule = b.split(RULE_SPLIT_PATTERN).filter(function(rule) {
+	          if (itt++ %2 == 0){
+	            return rule;
+	          }
+	          return '';
+	        });
+	        if (rule.length > 1) {
+	
+	          var value = rule[1].trim();
+	
+	          // Check if the value is a color (hex, rgb, or hsl)
+	          var colorMatch = COLOR_PATTERN.exec(value);
+	          if (colorMatch) {
+	
+	            // Format the color appropriately
+	            // TODO: make color formatting optional
+	            var color = new Color(colorMatch[0]);
+	            var colorString = color.alpha() === 1 ?
+	                color.hexString().toLowerCase() : color.rgbString();
+	
+	            // Increment count for the color
+	            if (!colors[colorString]) {
+	              colors[colorString] = 0;
+	            }
+	            colors[colorString] += 1;
+	            value = value.replace(colorMatch[0], colorString);
+	          }
+	
+	          a[rule[0].trim()] = value;
+	        }
+	        return a;
+	      }, {});
+	
+	      //store the comments
+	      var internalComments = [];
+	      var commentMatch = COMMENT_PATTERN.exec(cssStyles);
+	
+	      while (commentMatch) {
+	        internalComments.push(commentMatch[0].trim());
+	        commentMatch = COMMENT_PATTERN.exec(cssStyles);
+	      }
+	
+	      // Assign styles to the selector or define a new selector
+	      if (styles[selector]) {
+	        styles[selector].rules = Object.assign(styles[selector].rules, rules);
+	      } else {
+	        styles[selector] = {
+	          index: match.index,
+	          rules: rules,
+	          internalComments:internalComments
+	        };
+	      }
       }
       match = SELECTOR_PATTERN.exec(css);
     }
@@ -155,26 +159,16 @@ var fishsticss = {
     }
     return styles;
   },
-
-  prepare: function(css) {
-
-    // Scrape the CSS for styles and colors
-    var scrapings = this._scrape(css);
-    var styles = this._rinse(this._scrub(scrapings.styles));
-    var colors = [];
-
-    // Create array of colors that occur more than once
-    for (var color in scrapings.colors) {
-      if (scrapings.colors[color] > 0) {
-        colors.push(color);
-      }
-    }
-
-
-    // Get the comments
+  _prepareComments: function(css){
+    //Rebuild the comment map and comment data
     var comments = [];
+    var commentMap = new Array(css.length);
     var match = COMMENT_PATTERN.exec(css);
     while (match) {
+      //each part of the comment is marked in the matrix
+      for(var i=match.index;i<match[0].length;i++){
+    	  commentMap[i]=true;
+      }
       comments.push({
         index: match.index,
         text: match[0].trim()
@@ -182,11 +176,68 @@ var fishsticss = {
       match = COMMENT_PATTERN.exec(css);
     }
     
-    
     return {
+    	comments:comments,
+    	commentMap:commentMap
+    }
+  },
+  _processColors: function(colors , sourceColors){
+    // Create array of colors that occur more than once
+    for (var color in sourceColors) {
+      if (sourceColors[color] > 0) {
+    	  if (colors.indexOf(color)<0){
+    		  colors.push(color);
+    	  }
+      }
+    }
+    return colors;
+  },
+  prepare: function(css) {
+    // Get the comments
+	var commentData = this._prepareComments(css);
+    
+    var mediaBlocks = [];
+    //Extract the media and font 
+    var match = MEDIA_PATTERN.exec(css);
+    while (match) {
+    	if (commentData.commentMap[match.index] == undefined){
+    		var matchedMediaQuery = match[0].trim();
+    		var matchedCss = matchedMediaQuery.slice(matchedMediaQuery.indexOf("{")).trim('}');
+    		//Not in a comment
+    		var mediaScrapings = this._scrape(matchedCss ,[]);
+    		mediaBlocks.push({
+    			mediaQuery : matchedMediaQuery.slice(0, matchedMediaQuery.indexOf("{")),
+    			scrapings:mediaScrapings,
+    			styles:this._rinse(this._scrub(mediaScrapings.styles))
+    		});
+    		
+    	}
+    	match = MEDIA_PATTERN.exec(css);
+    }
+    
+    //Remove the media queries
+    var filteredCss = css.replace(MEDIA_PATTERN, '\n');
+
+    //Re-assess the comments
+    commentData = this._prepareComments(filteredCss);
+    
+    // Scrape the CSS for styles and colors
+    var scrapings = this._scrape(filteredCss , commentData.commentMap);
+    var styles = this._rinse(this._scrub(scrapings.styles));
+    
+    var colors = [];
+
+    colors = this._processColors(colors , scrapings.colors);
+    
+    for (var media in mediaBlocks) {
+    	colors = this._processColors(colors , mediaBlocks[media].scrapings.colors);
+    }
+
+    return {
+      mediaBlocks : mediaBlocks,
       styles: styles,
       colors: colors,
-      comments: comments
+      comments: commentData.comments
     };
   },
 
@@ -290,11 +341,24 @@ var fishsticss = {
     // useTabs [bool] If you prefer to use tabs rather than spaces
 
     var results = this.prepare(css);
-    return this.print(results.styles,
+    var output =  this.print(results.styles,
         options && !options.createVariables ? null : results.colors,
         options && !options.includeComments ? null : results.comments,
         options,
         true);
+    
+    
+    for (var media in results.mediaBlocks) {
+    	output += results.mediaBlocks[media].mediaQuery 
+    		+ '{\n'
+    		+ this.print(results.mediaBlocks[media].styles,
+    		       options && !options.createVariables ? null : results.colors,
+    		       options && !options.includeComments ? null : results.mediaBlocks[media].comments,
+    		       options,false)
+    		+ '\n}\n';
+    }
+    
+    return output;
   }
 };
 
