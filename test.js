d4a062a1e01131b26e206258be2d0d0f5177f08c
Trevor Blades <tdblades@gmail.com>
2018-07-29 15:07:02 -0700

    Add a bunch of tests

--- test.js
+++ test.js
@@ -1,76 +1,218 @@
+const outdent = require('outdent');
 const parse = require('./');
 
-const css = `
-  /* Comments ain't no thang! */
-  #id {
-    width: 100%;
-    color: rgb(0, 0,15);
-  }
-  
-  #id {
-    height: 100%;
-  }
+test('handles multiple levels of descendant selectors', () => {
+  const css = `
+    #id {
+      width: 420px;
+      color: green;
+    }
 
-  #id .class, #id .other .hehe {
-    height: 100%;
-    border: 1px solid #ff0000;
-  }
+    #id .child-class {
+      overflow: hidden;
+    }
 
-  #id .class {
-    color: red;
-  }
-  
-  /* A comment before a child class */
-  #id  .class  .child-class {
-    margin-top: 24px;
-  }
-  
-  #id .class .child-class2.lol {
-    width: 250px;
-  }
+    #id .child-class p {
+      margin: 0 1em;
+    }
 
-  /* A comment before a sub class */
-  #id .class.sub-class {
-    color: #ff0000;
-  }
+    #id .child-class p:last-child {
+      margin-bottom: 0;
+    }
+  `;
 
-  .another.random-thing {
-    color: green;
-  }
-`;
-
-test('parses css', () => {
   const actual = parse(css);
-  const expected = `#id {
-  width: 100%;
-  color: rgb(0, 0,15);
-  height: 100%;
+  const expected = outdent`
+    #id {
+      width: 420px;
+      color: green;
 
-  .class, .other .hehe {
-    height: 100%;
-    border: 1px solid #ff0000;
-  }
+      .child-class {
+        overflow: hidden;
 
-  .class {
-    color: red;
+        p {
+          margin: 0 1em;
 
-    .child-class2.lol {
-      width: 250px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+        }
+      }
     }
-
-    .child-class {
-      margin-top: 24px;
-    }
-
-    &.sub-class {
-      color: #ff0000;
-    }
-  }
-}
-
-.another.random-thing {
-  color: green;
-}
-`;
+  `;
   expect(actual).toBe(expected);
+});
+
+describe('formats css with weird or inconsistent spacing', () => {
+  test('differences in spacing among rules', () => {
+    const css = `
+      #id {width: 420px; color: green;}
+      .another-class {
+        margin-top: 4px;
+      }
+    `;
+
+    const actual = parse(css);
+    const expected = outdent`
+      #id {
+        width: 420px;
+        color: green;
+      }
+
+      .another-class {
+        margin-top: 4px;
+      }
+    `;
+
+    expect(actual).toBe(expected);
+  });
+
+  test('extra space between selectors, declarations, and properties', () => {
+    const css = `
+      #id {
+        position:   relative;
+      }
+      #id    .child-class {position:absolute;
+        top: 24px;
+      }
+    `;
+
+    const actual = parse(css);
+    const expected = outdent`
+      #id {
+        position: relative;
+
+        .child-class {
+          position: absolute;
+          top: 24px;
+        }
+      }
+    `;
+
+    expect(actual).toBe(expected);
+  });
+
+  test('extra whitespace before, after, and between rules', () => {
+    const css = `
+
+
+      #id {width: 420px; color: green;}
+
+
+      #id p {
+        color: grey;
+
+
+      }
+
+
+      .another-class {
+        margin-top: 4px;
+      }
+
+
+    `;
+
+    const actual = parse(css);
+    const expected = outdent`
+      #id {
+        width: 420px;
+        color: green;
+
+        p {
+          color: grey;
+        }
+      }
+
+      .another-class {
+        margin-top: 4px;
+      }
+    `;
+
+    expect(actual).toBe(expected);
+  });
+});
+
+test('does not nest selectors unless their parents have styles', () => {
+  const css = `
+    #id {
+      width: 420px;
+      color: green;
+    }
+
+    #id .child-class p:last-child {
+      margin-bottom: 0;
+    }
+  `;
+
+  const actual = parse(css);
+  const expected = outdent`
+    #id {
+      width: 420px;
+      color: green;
+
+      .child-class p:last-child {
+        margin-bottom: 0;
+      }
+    }
+  `;
+
+  expect(actual).toBe(expected);
+});
+
+describe('accepts different indentation options', () => {
+  const css = `
+    #id {
+      width: 420px;
+      color: green;
+    }
+
+    #id .child-class {
+      overflow: hidden;
+    }
+
+    #id .child-class p {
+      margin: 0 1em;
+    }
+  `;
+
+  test('variable number of spaces', () => {
+    const actual = parse(css, {spaces: 3});
+    const expected = outdent`
+      #id {
+         width: 420px;
+         color: green;
+
+         .child-class {
+            overflow: hidden;
+
+            p {
+               margin: 0 1em;
+            }
+         }
+      }
+    `;
+
+    expect(actual).toBe(expected);
+  });
+
+  test('use tabs', () => {
+    const actual = parse(css, {spaces: false});
+    const expected = outdent`
+      #id {
+      \twidth: 420px;
+      \tcolor: green;
+
+      \t.child-class {
+      \t\toverflow: hidden;
+
+      \t\tp {
+      \t\t\tmargin: 0 1em;
+      \t\t}
+      \t}
+      }
+    `;
+
+    expect(actual).toBe(expected);
+  });
 });
