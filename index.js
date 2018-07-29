24ffd8467edfb41a8ccfea3cea92d45a0c053b6d
Trevor Blades <tdblades@gmail.com>
2018-07-29 16:01:53 -0700

    Transpile code for use in browsers

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const css = require('css');
-const indentString = require('indent-string');
-const fromPairs = require('lodash/fromPairs');
-const map = require('lodash/map');
-const orderBy = require('lodash/orderBy');
-const reject = require('lodash/reject');
-const repeat = require('lodash/repeat');
-const uniq = require('lodash/uniq');
+import css from 'css';
+import indentString from 'indent-string';
+import fromPairs from 'lodash/fromPairs';
+import map from 'lodash/map';
+import orderBy from 'lodash/orderBy';
+import reject from 'lodash/reject';
+import repeat from 'lodash/repeat';
+import uniq from 'lodash/uniq';
 
 const SPACE = ' ';
 const COMMA_SPACE = ', ';
@@ -87,7 +87,7 @@ function render(styles, count, options) {
   return text;
 }
 
-module.exports = function(code, options = {}) {
+export default function parse(code, options = {}) {
   const parsed = css.parse(code);
   const rules = reject(parsed.stylesheet.rules, 'comment');
   const styles = rules.reduce((obj, rule) => {
@@ -173,4 +173,4 @@ module.exports = function(code, options = {}) {
 
   const cleaned = cleanup(styles);
   return render(cleaned, 0, options).replace(/\s+$/, '');
-};
+}
