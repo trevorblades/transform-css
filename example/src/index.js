b03453a20fceb52e0a22d832208cefa47da6ba22
Trevor Blades <tdblades@gmail.com>
2018-07-29 22:46:05 -0700

    Use the shared mui theme

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,12 @@
 import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import theme from '@trevorblades/mui-theme';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
